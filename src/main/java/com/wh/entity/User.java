a07060debbd8b9ba8278ac4ef59e0b6dcaf4013a
whArchitect <2074188684@qq.com>
2019-10-28 17:50:35 +0800

    1 2

---
+++ src/main/java/com/wh/entity/User.java
@@ -0,0 +1,22 @@
+package com.wh.entity;
+
+public class User {
+    private String name;
+    private String psd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPsd() {
+        return psd;
+    }
+
+    public void setPsd(String psd) {
+        this.psd = psd;
+    }
+}
