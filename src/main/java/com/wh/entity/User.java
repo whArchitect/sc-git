df160987ca80a53f0e05b9cb0237706e636c327e
whArchitect <2074188684@qq.com>
2019-10-28 18:17:06 +0800

    qw

--- src/main/java/com/wh/entity/User.java
+++ src/main/java/com/wh/entity/User.java
@@ -6,6 +6,22 @@ public class User {
     private String email;
     private String phone;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     public String getName() {
         return name;
     }
